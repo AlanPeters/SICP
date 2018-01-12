92bb1321b25251cfe3a2d66660798e898c0135bb
Alan Peters <alan3051@gmail.com>
2018-01-11 19:01:29 -0700

    Finishing 1.16

---
+++ Chapter_1/Exercise_1_16.scm
@@ -0,0 +1,13 @@
+(define (fast-expt b n)
+  (expt-iter b n 1)
+  )
+
+(define (expt-iter b n a)
+  (cond ((= n 0) a)
+        ((= n 1) (* a b))
+        ((is-even n) (expt-iter (* b b) (/ n 2) a ))
+        (else (expt-iter b (- n 1) (* a b)))
+        )
+  )
+
+(define (is-even n) (= (remainder n 2) 0))
