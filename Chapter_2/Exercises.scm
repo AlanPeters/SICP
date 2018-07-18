7bdb23ff05cd28a96f68600a3089a3351aa777d9
Alan Peters <alan3051@gmail.com>
2018-07-18 12:55:13 -0600

    Adding solution for exercise 2.2

--- Chapter_2/Exercises.scm
+++ Chapter_2/Exercises.scm
@@ -1,15 +1,17 @@
+(define (abs x)
+  (if (< x 0) (- x) x))
+(define (average a b)
+  (/ (+ a b) 2))
                                         ;Section 2.1
                                         ;Section 2.1.1
 ;; (define (make-rat n d )
   ;; (cons n d))
-(define (abs x)
-  (if (< x 0) (- x) x))
 
 (define (gcd a b)
   (if (= b 0)
       a
       (gcd b (remainder a b))))
-
+                                        ;Exercise 2.1
 (define (make-rat n d)
   (define (make-rat-inner n1 d1)
     (let ((g (abs (gcd n1 d1))))
@@ -49,3 +51,34 @@
   (display "/")
   (display (denom x))
   (newline))
+
+                                        ;Exercise 2.2
+(define (make-point x y) (cons x y))
+
+(define (x-point p) (car p))
+
+(define (y-point p) (cdr p))
+
+(define (make-segment start end) (cons start end))
+
+(define (start-segment s) (car s))
+(define (end-segment s) (cdr s))
+
+(define (print-point p)
+  (display "(")
+  (display (x-point p))
+  (display ",")
+  (display (y-point p))
+  (display ")")
+  (newline))
+
+(define (midpoint s)
+  (make-point (average (x-point (start-segment s))
+                       (x-point (end-segment s))
+                       )
+              (average (y-point (start-segment s))
+                       (y-point (end-segment s)))))
+
+(define (exercise_2.2)
+  (print-point (midpoint (make-segment (make-point 1 1) (make-point 2 2))))
+  )
