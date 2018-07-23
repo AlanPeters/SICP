ccbfdd160ee36357e395be6defc6d54a28fc1498
Alan Peters <alan3051@gmail.com>
2018-07-23 13:22:42 -0600

    Renaming exercises file and adding solution for 1.19

--- Chapter_1/Exercises_Chapter_1.scm
+++ Chapter_1/Exercises_Chapter_1.scm
@@ -1,3 +1,42 @@
+
+;Exercise 1.19
+(define (fib n)
+  (fib-iter 1 0 0 1 n))
+
+(define (fib-iter a b p q count)
+  (cond ((= count 0 ) b)
+        ((even? count)
+         (fib-iter a
+                   b
+                   (+ (* q q) (* p p))   ;p'
+                   (+ (* 2 p q) (* q q)) ;q'
+                   (/ count 2)))
+        (else (fib-iter (+ (* b q) (* a q) (* a p))
+                        (+ (* b p) (* a q))
+                        p
+                        q
+                        (- count 1)))))
+#|
+a <- bq + aq + ap
+b <- bp + aq
+
+b' <- (bp + aq)p + (bq + aq + ap)q
+b' <- bp^2 + aqp + bq^2 + aq^2 + apq
+b' <- bp^2 + 2aqp + bq^2 + aq^2
+;b' <- b(p') + a(q')
+b' <- b(p^2 + q^2) + a(2qp + q^2)
+
+a' <-(bp + aq)q + (bq + aq + ap)q + (bq + aq + ap)p
+a' <- bpq + aq^2 + bq^2 + aq^2 + apq + bqp + aqp + ap^2
+a' <- 2bpq + 2aq^2 + bq^2 + 2apq + ap^2
+;a' <- b(q') + a(q') + a(p')
+a' <- b(2pq + q^2) + a(2pq + q^2) + a(q^2 + p^2)
+
+q' <- 2pq + q^2
+p' <- q^2 + p^2
+
+|#
+
 (define (f g)
   (g 2))
 
